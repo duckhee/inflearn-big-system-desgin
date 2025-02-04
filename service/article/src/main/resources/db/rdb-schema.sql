bfab75f191f625ffffa2cc9dc0f88815ea44089e
DoukheeWon <fain9301@yahoo.com>
2025-02-04 14:42:38 +0900

    feat: snowflake set

---
+++ service/article/src/main/resources/db/rdb-schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE tbl_article
+(
+    article_id  BIGINT        NOT NULL PRIMARY KEY COMMENT "This is article ID",
+    title       VARCHAR(100)  NOT NULL COMMENT "article title",
+    content     VARCHAR(3000) NOT NULL COMMENT "article content",
+    board_id    BIGINT        NOT NULL COMMENT "Board ID",
+    writer_id   BIGINT        NOT NULL COMMENT "writer ID",
+    created_at  DATETIME      NOT NULL DEFAULT NOW() COMMENT "article create time",
+    modified_at DATETIME      NOT NULL DEFAULT NULL COMMENT "article modified time"
+)

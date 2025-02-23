c013dbf0bce147a4bd5989e5317f5e8c6650b98c
DoukheeWon <fain9301@yahoo.com>
2025-02-23 16:19:50 +0900

    feat: set comment table

--- service/article/src/main/resources/db/rdb-schema.sql
+++ service/article/src/main/resources/db/rdb-schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE tbl_article
     article_id  BIGINT        NOT NULL PRIMARY KEY COMMENT "This is article ID",
     title       VARCHAR(100)  NOT NULL COMMENT "article title",
     content     VARCHAR(3000) NOT NULL COMMENT "article content",
-    board_id    BIGINT        NOT NULL COMMENT "Board ID",
+    board_id    BIGINT        NOT NULL COMMENT "Board ID (Shard Key)",
     writer_id   BIGINT        NOT NULL COMMENT "writer ID",
     created_at  DATETIME      NOT NULL DEFAULT NOW() COMMENT "article create time",
     modified_at DATETIME      NOT NULL COMMENT "article modified time"
@@ -11,3 +11,15 @@ CREATE TABLE tbl_article
 
 /*날짜에 대한 Index 를 생성하는 Query */
 # CREATE INDEX idx_board_id_article_id ON tbl_article (board_id ASC, article_id DESC);
+
+create table tbl_comment
+(
+    comment_id        BIGINT        NOT NULL PRIMARY KEY COMMENT "This is comment ID",
+    content           VARCHAR(3000) NOT NULL COMMENT "This is comment content",
+    article_id        BIGINT        NOT NULL COMMENT "article ID(Shard Key)",
+#     parent_comment_id BIGINT COMMENT "Parent comment id",
+    parent_comment_id BIGINT        NOT NULL COMMENT "Parent comment id",
+    writer_id         BIGINT        NOT NULL COMMENT "comment write user id",
+    deleted           BOOL          NOT NULL DEFAULT FALSE COMMENT "comment delete or not flag",
+    created_at        DATETIME      NOT NULL COMMENT "comment write time"
+);
